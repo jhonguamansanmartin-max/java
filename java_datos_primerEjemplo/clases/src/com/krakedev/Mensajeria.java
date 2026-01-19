c249035e9f51c5de277039e7575a645f2a4a1ca9
jhonguamansanmartin-max <jhonguamansamartin@gmail.com>
2026-01-19 01:46:48 -0500

    se agrega las funciones del reto '9'

---
+++ java_datos_primerEjemplo/clases/src/com/krakedev/Mensajeria.java
@@ -0,0 +1,12 @@
+package com.krakedev;
+
+public class Mensajeria {
+	public void saludar() {
+		System.out.println("hola mundo");
+		
+	}
+	public void saludarAmigo(String nombre,String apellido ) {
+			System.out.println("hola " + nombre +" "+apellido);
+		}
+	
+}
