a5d041e0d4abf8edfa91503a2852693e487a1603
jhonguamansanmartin-max <jhonguamansamartin@gmail.com>
2026-01-22 00:16:50 -0500

    se agrega calcular perimetro

---
+++ java_datos_primerEjemplo/clases/src/com/krakedev/Rectangulo.java
@@ -0,0 +1,22 @@
+package com.krakedev;
+
+public class Rectangulo {
+	public int base;
+	public int altura;
+	
+	
+	public int calcularArea() {
+		int area;
+		area=base*altura;	
+		return area;
+	}
+	
+	public double calcularPerimetro() {
+		int perimetro;
+		perimetro=2*(base+altura);	
+		return perimetro;
+	}
+	
+	
+
+}
